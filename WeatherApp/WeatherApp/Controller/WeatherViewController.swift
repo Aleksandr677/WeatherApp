77eab4330e67215dd1e7a3279dea9c7b31944550
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-23 18:05:37 +0300

    feat: create the storyboard and add the IBOutlets

---
+++ WeatherApp/WeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Александр Христиченко on 23.05.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+

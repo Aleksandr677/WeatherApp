fc2ce43e5ddace9d6e2786fcc60a5ca200ebfb3f
Aleksandr Khristichenko <alexanderhristichenko@yandex.ru>
2022-05-23 18:50:47 +0300

    feat: add delegate methods for the textfield

--- WeatherApp/WeatherApp/Controller/WeatherViewController.swift
+++ WeatherApp/WeatherApp/Controller/WeatherViewController.swift
@@ -12,9 +12,37 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) //for dismissing the keyboard
     }
 }
 
+
+//MARK: - TextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true) //for dismissing the keyboard
+        return true //return button
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something here"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = "" //for clearing textField after typing
+    }
+}
